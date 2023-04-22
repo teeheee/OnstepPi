50f4612d2fabbe0cf33a681bfcdc008416d8de85
Alexander ulbrich <alex.ulbrich.ulm@gmail.com>
2023-04-22 17:19:33 +0200

    increase SoftSPI delay

--- src/lib/softSpi/SoftSpi.cpp
+++ src/lib/softSpi/SoftSpi.cpp
@@ -4,6 +4,8 @@
 #include "../../Common.h"
 #include "SoftSpi.h"
 
+#define SPI_DELAY 100
+
 bool SoftSpi::init(int16_t mosi, int16_t sck, int16_t cs, int16_t miso) {
   this->miso = miso;
   this->mosi = mosi;
@@ -23,26 +25,26 @@ bool SoftSpi::init(int16_t mosi, int16_t sck, int16_t cs, int16_t miso) {
 void SoftSpi::begin() {
   pinModeEx(cs, OUTPUT);
   digitalWriteEx(cs, HIGH);
-  delayMicroseconds(1);
+  delayMicroseconds(SPI_DELAY);
   pinMode(sck, OUTPUT);
   digitalWriteF(sck, HIGH);
   pinMode(miso, INPUT);
   pinMode(mosi, OUTPUT);
-  delayMicroseconds(1);
+  delayMicroseconds(SPI_DELAY);
   digitalWriteEx(cs, LOW);
-  delayMicroseconds(1);
+  delayMicroseconds(SPI_DELAY);
 }
 
 void SoftSpi::pause() {
   digitalWriteEx(cs, HIGH);
-  delayMicroseconds(1);
+  delayMicroseconds(SPI_DELAY);
   digitalWriteEx(cs, LOW);
-  delayMicroseconds(1);
+  delayMicroseconds(SPI_DELAY);
 }
 
 void SoftSpi::end() {
   digitalWriteEx(cs, HIGH);
-  delayMicroseconds(1);
+  delayMicroseconds(SPI_DELAY);
 }
 
 uint8_t SoftSpi::transfer(uint8_t data_out) {
@@ -50,10 +52,10 @@ uint8_t SoftSpi::transfer(uint8_t data_out) {
   for(int i = 7; i >= 0; i--) {
     digitalWriteF(sck, LOW);
     digitalWriteF(mosi, bitRead(data_out, i));
-    delayMicroseconds(1);
+    delayMicroseconds(SPI_DELAY);
     digitalWriteF(sck, HIGH);
     bitWrite(data_in, i, digitalReadF(miso));
-    delayMicroseconds(1);
+    delayMicroseconds(SPI_DELAY);
   }
   return data_in;
 }
@@ -63,10 +65,10 @@ uint32_t SoftSpi::transfer32(uint32_t data_out) {
   for(int i = 31; i >= 0; i--) {
     digitalWriteF(sck, LOW);
     digitalWriteF(mosi, bitRead(data_out, i));
-    delayMicroseconds(1);
+    delayMicroseconds(SPI_DELAY);
     digitalWriteF(sck, HIGH);
     bitWrite(data_in, i, digitalReadF(miso));
-    delayMicroseconds(1);
+    delayMicroseconds(SPI_DELAY);
   }
   return data_in;
 }
